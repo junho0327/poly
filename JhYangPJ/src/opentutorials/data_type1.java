0407863736310f91cacc2cabf0cf6a1a0d3e8eb5
junho0327 <mikey1307@naver.com>
2021-03-23 10:59:57 +0900

    숫자와 문자

---
+++ JhYangPJ/src/opentutorials/data_type1.java
@@ -0,0 +1,22 @@
+package opentutorials;
+
+public class data_type1 {
+
+	public static void main(String[] args) {
+		//숫자
+		System.out.println(1+2);
+		System.out.println(1.2+1.3);
+		System.out.println(2*5);
+		System.out.println(6/2);
+		
+		//문자와 문자열
+		System.out.println('생'); //문자 (한단어)
+		System.out.println("생활코딩"); //문자열
+		System.out.println("생");
+		
+		System.out.println("egoing said \"Welcome programming world\""); //큰 따음표 표시
+		System.out.println("HTML\nCSS\nJavaScript\n"); //여러줄 표시
+		System.out.println("생활"+"코딩"); //문자+문자
+	}
+
+}
