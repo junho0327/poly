b081d2e701c6ee8bf108b88db5a512e166a7a938
junho0327 <mikey1307@naver.com>
2021-03-24 10:54:03 +0900

    실습 5-1 기본 if 문 사용 예 1

---
+++ JhYangPJ/src/Java_exercise/Ex05_01.java
@@ -0,0 +1,16 @@
+package Java_exercise;
+
+public class Ex05_01 {
+
+	public static void main(String[] args) {
+		int a = 100;
+				
+		if (a <100) {
+			System.out.println("100보다 작군요..");
+		}
+		
+		
+		System.out.println("난 무조건 실행된다.");
+	}
+
+}
