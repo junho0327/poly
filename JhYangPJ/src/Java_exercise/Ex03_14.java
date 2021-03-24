62c79eeb0ca26bb0151371376d77e1f8cbc07936
junho0327 <mikey1307@naver.com>
2021-03-24 09:30:34 +0900

    실습 3-14 불형 사용 예

---
+++ JhYangPJ/src/Java_exercise/Ex03_14.java
@@ -0,0 +1,15 @@
+package Java_exercise;
+
+public class Ex03_14 {
+
+	public static void main(String[] args) {
+		boolean boo1, boo2;
+		
+		boo1 = true;
+		System.out.println("boo1: " +boo1);
+		
+		boo2 = (10 == 20);
+		System.out.printf("boo2: " +boo2);
+	}
+
+}
