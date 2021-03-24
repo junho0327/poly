0d045439b3c33f6918601886df28ca53f5acbee9
junho0327 <mikey1307@naver.com>
2021-03-24 10:12:45 +0900

    실습 4-1 산술 연산자 사용 예

---
+++ JhYangPJ/src/Java_exercise/Ex04_1.java
@@ -0,0 +1,25 @@
+package Java_exercise;
+
+public class Ex04_1 {
+
+	public static void main(String[] args) {
+		int a, b = 5, c = 3;
+
+		a = b + c;
+		System.out.println(b + " + " + c + " = " + a);
+
+		a = b - c;
+		System.out.println(b + " - " + c + " = " + a);
+
+		a = b * c;
+		System.out.println(b + " * " + c + " = " + a);
+
+		a = b / c;
+		System.out.println(b + " / " + c + " = " + a);
+
+		a = b % c;
+		System.out.println(b + " % " + c + " = " + a);
+
+	}
+
+}
