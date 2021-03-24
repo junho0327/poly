290695f8d92d9c1626ec578f9a1dd0ad6895d9d5
junho0327 <mikey1307@naver.com>
2021-03-24 09:28:08 +0900

    실습 3-7 변수에 값을 대입

---
+++ JhYangPJ/src/Ex03_07.java
@@ -0,0 +1,16 @@
+
+public class Ex03_07 {
+
+	public static void main(String[] args) {
+		int a;
+		float b;
+		
+		a = (int) 123.45f;
+		
+		b = 200;
+		
+		System.out.printf("a�� �� ==> %d \n",a);
+		System.out.printf("b�� �� ==> %f \n",b);
+	}
+
+}
