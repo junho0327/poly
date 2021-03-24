c07c95166bb190953dbba47e5a46a5a83fa7808d
junho0327 <mikey1307@naver.com>
2021-03-24 10:05:22 +0900

    실습 3-15 문자열 사용 예

---
+++ JhYangPJ/src/Java_exercise/Ex03_15.java
@@ -0,0 +1,21 @@
+package Java_exercise;
+
+public class Ex03_15 {
+
+	public static void main(String[] args) {
+		String str1 = "IT Codebook 입니다.";
+
+		String str2 = "10";
+		String str3 = "20";
+
+		str1 = "Java 입니다.";
+
+		System.out.println("str1: " + str1);
+		System.out.println("더하기 결과: " + str2 + str3);
+
+		int num1 = Integer.parseInt(str2);
+		int num2 = Integer.parseInt(str3);
+		System.out.println("더하기 결과: " + (num1 + num2));
+	}
+
+}
