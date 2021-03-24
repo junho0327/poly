a838ec44462b7e742347491c5ef1bcc9f809a737
junho0327 <mikey1307@naver.com>
2021-03-24 09:33:33 +0900

    실습 3-7

--- JhYangPJ/src/Java_exercise/Ex03_07.java
+++ JhYangPJ/src/Java_exercise/Ex03_07.java
@@ -3,8 +3,15 @@ package Java_exercise;
 public class Ex03_07 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int a;
+		float b;
+		
+		a = (int) 123.45f;
+		
+		b = 200;
+		
+		System.out.printf("a�� �� ==> %d \n",a);
+		System.out.printf("b�� �� ==> %f \n",b);
 	}
 
 }
