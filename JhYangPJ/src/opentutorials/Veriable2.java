3edce0d5d5a5b7cdb5377bcb7638c520ee4ae2ca
junho0327 <mikey1307@naver.com>
2021-03-25 11:37:16 +0900

    변수

---
+++ JhYangPJ/src/opentutorials/Veriable2.java
@@ -0,0 +1,13 @@
+package opentutorials;
+
+public class Veriable2 {
+
+	public static void main(String[] args) {
+		double a = 1.1;
+		System.out.println(a+1.1); //2.2
+		
+		a=2.1;
+		System.out.println(a+1.1); //3.2
+	}
+
+}
---
+++ JhYangPJ/src/opentutorials/Veriable3.java
@@ -0,0 +1,17 @@
+package opentutorials;
+
+public class Veriable3 {
+
+	public static void main(String[] args) {
+		String first = "coding";
+		System.out.println(first+" "+"everybody");
+		
+		String a,b;
+		a = "coding";
+		b = " everybody";
+		System.out.println(a+b);
+		
+		
+	}
+
+}
