8042827666fc3e688d9beee7116387722da8bc26
junho0327 <mikey1307@naver.com>
2021-03-26 12:16:50 +0900

    주석과 세미콜론

---
+++ JhYangPJ/src/opentutorials/Comment1.java
@@ -0,0 +1,18 @@
+package opentutorials;
+
+public class Comment1 {
+
+	public static void main(String[] args) {
+		// 두개의 변수가 같은 데이터 타입 일 때 아래와 같이 코드를 작성한다.
+	    String a, b;
+	    /*
+	    a = "coding";
+	    b = "everybody";
+	    System.out.println(a+b);
+	    */
+	    a = "coding";
+	    b = "everybody";
+	    System.out.println(a+b);
+	}
+
+}
