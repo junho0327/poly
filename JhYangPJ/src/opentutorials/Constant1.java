1898ebf27e8f8fb7994d2c8fa48a8cd2624c7e36
junho0327 <mikey1307@naver.com>
2021-03-30 13:25:56 +0900

    상수의 데이터 타입

---
+++ JhYangPJ/src/opentutorials/Constant1.java
@@ -0,0 +1,31 @@
+package opentutorials;
+
+public class Constant1 {
+
+	public static void main(String[] args) {
+		int a = 1;
+		System.out.println(a);
+		
+		int a1 = (int) 2.2;
+		System.out.println(a1);
+		
+		float a2 = (float) 2.2;
+		System.out.println(a2);
+		
+		double a3 = 2.2;
+		System.out.println(a3);
+		
+		float a4 = 2.2F;
+		System.out.println(a4);
+		
+
+		long a7 = 2147483648L;
+		System.out.println(a7);
+		
+		byte a8 = 100;
+		short b = 200;
+		System.out.println(a8);
+		System.out.println(b);
+	}
+
+}
