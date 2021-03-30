7f7956b48542f5e167c94fb5431c7ee0aa18b51d
junho0327 <mikey1307@naver.com>
2021-03-30 13:30:28 +0900

    데이터 타입

--- JhYangPJ/src/opentutorials/Data_type2.java
+++ JhYangPJ/src/opentutorials/Data_type2.java
@@ -3,18 +3,24 @@ package opentutorials;
 public class Data_type2 {
 
 	public static void main(String[] args) {
-		byte a;
-		
+		byte a = 0;
+		System.out.println(a);
 		byte district = 1; //경기도
+		System.out.println(district);
 		long district1 = 1;
+		System.out.println(district1);
 		int population = 50000000;
+		System.out.println(population);
 		
 		long a1 = 2147483647;
+		System.out.println(a1);
 		long b = 1;
+		System.out.println(b);
 		
 		int a2 = 2147483647;
+		System.out.println(a2);
 		long b1 = 2147483647;
-		
+		System.out.println(b1);
 		
 	}
 
