b865fed23ae9c90e6a8b3b5654e7636a7c3d6104
junho0327 <mikey1307@naver.com>
2021-03-30 13:16:25 +0900

    데이터 타입

---
+++ JhYangPJ/src/opentutorials/Data_type2.java
@@ -0,0 +1,21 @@
+package opentutorials;
+
+public class Data_type2 {
+
+	public static void main(String[] args) {
+		byte a;
+		
+		byte district = 1; //경기도
+		long district1 = 1;
+		byte population = 50000000;
+		
+		long a1 = 2147483647;
+		long b = 1;
+		
+		int a2 = 2147483647;
+		long b1 = 2147483647;
+		
+		
+	}
+
+}
