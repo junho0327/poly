b973b947c53e7dc6a1cd3de7fdb886fda1927746
junho0327 <mikey1307@naver.com>
2021-03-31 11:02:12 +0900

    실습 5-4

---
+++ JhYangPJ/src/Java_exercise/Ex05_4.java
@@ -0,0 +1,18 @@
+package Java_exercise;
+
+public class Ex05_4 {
+
+	public static void main(String[] args) {
+		int a = 200;
+		
+		if (a<100) {
+			System.out.println("100보다 작군요..");
+			System.out.println("참이면 보이죠?");
+		} else {
+			System.out.println("100보다 크군요..");
+			System.out.println("거짓이면 보이죠?");
+		}
+		System.out.println("난 조건문에 상관없이 무조건 보인다.");
+	}
+
+}
