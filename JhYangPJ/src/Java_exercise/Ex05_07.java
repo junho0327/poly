71290dc2899a57303b86f2e09de9c6bfb6a23196
junho0327 <mikey1307@naver.com>
2021-03-31 10:46:51 +0900

    실습 5-7 중첩 if문 사용 예1

---
+++ JhYangPJ/src/Java_exercise/Ex05_07.java
@@ -0,0 +1,21 @@
+package Java_exercise;
+
+public class Ex05_07 {
+
+	public static void main(String[] args) {
+		int a=75;
+		
+		if (a>50) {
+			if (a<100) {
+				System.out.println(a+"는 50보다 크고 100보다 작군요");
+			} else {
+				System.out.println(a + "는 100보다 크군요");
+			}
+			
+		} else {
+			System.out.println(a+"는 50보다 작군요");
+		}
+		
+	}
+
+}
