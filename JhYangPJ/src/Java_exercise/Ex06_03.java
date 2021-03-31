636bb45110af4a7ebd9b13e21a79d6dbbd062756
junho0327 <mikey1307@naver.com>
2021-03-31 11:22:53 +0900

    실습 6-3 for문 중괄호 사용 예

---
+++ JhYangPJ/src/Java_exercise/Ex06_03.java
@@ -0,0 +1,22 @@
+package Java_exercise;
+
+public class Ex06_03 {
+
+	public static void main(String[] args) {
+
+		int i;
+
+		for (i = 0; i < 3; i++); {
+			System.out.println("반복을 위한 i 값: " +i);
+			System.out.println("또 안녕하세요?");
+		}
+
+		System.out.println();
+		System.out.println();
+
+		for (i = 1; i <= 3; i++); {
+			System.out.println("반복 횟수: " +i);
+		}
+	}
+
+}
