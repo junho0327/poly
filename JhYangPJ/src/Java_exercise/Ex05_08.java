f7e50197d0cb8e98544c84d565d71fd09e067458
junho0327 <mikey1307@naver.com>
2021-03-31 10:38:43 +0900

    실습 5-8 줄첩 if문 사용 예2

--- JhYangPJ/src/Java_exercise/Ex05_08.java
+++ JhYangPJ/src/Java_exercise/Ex05_08.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Ex05_08 {
 
 	public static void main(String[] args) {
+		@SuppressWarnings("resource")
 		Scanner s = new Scanner(System.in);
 		
 		int a;
@@ -12,21 +13,19 @@ public class Ex05_08 {
 		System.out.println("점수를 입력하세요: ");
 		a = s.nextInt();
 		
-		if(a>=90) {
+		if(a>=90) 
 			System.out.println("A");
-		}else 
-			if(a>=80) {
+		else 
+			if(a>=80) 
 				System.out.println("B");
-			}else 
-				if(a>=70) {
+			else 
+				if(a>=70) 
 					System.out.println("C");
-				}else 
-					if(a>=60) {
+				else 
+					if(a>=60) 
 						System.out.println("D");
-					}
+		
 					System.out.println("학점입니다.");
-				 
-			  
 			
 	}
 }
