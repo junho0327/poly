50c36c19f0d91e3f0704646a1931676f18da3644
junho0327 <mikey1307@naver.com>
2021-03-31 10:37:53 +0900

    실습 5-8 중첩if문 사용 예2

---
+++ JhYangPJ/src/Java_exercise/Ex05_08.java
@@ -0,0 +1,32 @@
+package Java_exercise;
+
+import java.util.Scanner;
+
+public class Ex05_08 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+		int a;
+		
+		System.out.println("점수를 입력하세요: ");
+		a = s.nextInt();
+		
+		if(a>=90) {
+			System.out.println("A");
+		}else 
+			if(a>=80) {
+				System.out.println("B");
+			}else 
+				if(a>=70) {
+					System.out.println("C");
+				}else 
+					if(a>=60) {
+						System.out.println("D");
+					}
+					System.out.println("학점입니다.");
+				 
+			  
+			
+	}
+}
