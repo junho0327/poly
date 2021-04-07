858869df360d3aa3d2f815251d6b113bebe4ecd4
junho0327 <mikey1307@naver.com>
2021-04-07 11:19:14 +0900

    실습 7-1

---
+++ JhYangPJ/src/Java_exercise/Ex07_01.java
@@ -0,0 +1,15 @@
+package Java_exercise;
+
+public class Ex07_01 {
+
+	public static void main(String[] args) {
+		int i;
+		i = 0;
+
+		while (i < 5) {
+			System.out.println("while문을 공부합니다.");
+			i++;
+		}
+	}
+
+}
