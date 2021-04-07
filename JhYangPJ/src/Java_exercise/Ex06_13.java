c14c65d4f38a6d18bd4a5dacae7dde5e56e2b6eb
junho0327 <mikey1307@naver.com>
2021-04-07 11:18:46 +0900

    실습 6-13

---
+++ JhYangPJ/src/Java_exercise/Ex06_13.java
@@ -0,0 +1,16 @@
+package Java_exercise;
+
+public class Ex06_13 {
+
+	public static void main(String[] args) {
+		int i, k;
+
+		for (i = 0; i < 3; i++) {
+
+			for (k = 0; k < 2; k++) {
+				System.out.println("[중첩For문] i값: " + i + " k값: " + k);
+			}
+		}
+	}
+
+}
