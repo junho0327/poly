f72af93274448247b5fbf736891396c87d6ea55a
junho0327 <mikey1307@naver.com>
2021-04-07 11:19:29 +0900

    실습 7-2

---
+++ JhYangPJ/src/Java_exercise/Ex07_02.java
@@ -0,0 +1,16 @@
+package Java_exercise;
+
+public class Ex07_02 {
+
+	public static void main(String[] args) {
+		int sum = 0;
+		int i=0;
+
+		while (i < 11); {
+			sum += i;
+			System.out.println("1 부터 10까지의 합 : " +sum);
+		}
+		
+	}
+
+}
