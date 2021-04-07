b3e54794e042a01f8f8044f41e6a08290ce4f657
junho0327 <mikey1307@naver.com>
2021-04-07 10:32:16 +0900

    쪽지시험1

---
+++ JhYangPJ/src/Ex1.java
@@ -0,0 +1,22 @@
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		int i;
+
+		for (i = 1; i < 11; i++) {
+			if (i % 3 == 0)
+				System.out.println("[3의배수] " + i);
+			else
+				System.out.println(+i);
+		}
+
+		for (i = 1; i < 11; i++) {
+			if (i % 3 == 0) {
+				System.out.println("[3의 배수]");
+			}
+			System.out.println(i);
+		}
+	}
+
+}
