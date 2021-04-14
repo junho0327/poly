194c52a4f5e22a28d5e54ec9c30c4eea7688a6a4
junho0327 <mikey1307@naver.com>
2021-04-14 09:34:58 +0900

    연습 7-12

--- JhYangPJ/src/Java_exercise/Ex07_12.java
+++ JhYangPJ/src/Java_exercise/Ex07_12.java
@@ -6,9 +6,8 @@ public class Ex07_12 {
 
 		int hap = 0;
 		int i = 0;
-
-		while(true) { 
-	//	for (;;) {
+ 
+		myLabel: for (;;) {
 			
 			for (i = 1; i <= 100; i++) {
 				hap += i;
@@ -16,11 +15,11 @@ public class Ex07_12 {
 				if (hap > 2000) {
 					System.out.println("합계: " + hap);
 					hap = 0;
-				//	break;
-					return;
+					break myLabel;
+//					return;
 				}
 			}
-			System.out.println("아직도 반복중...");
+			System.out.println("아직도 반복중..");
 		}
 	}
 }
